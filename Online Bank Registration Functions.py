8f251e7e62b4635fe86309a7ea0a577e6ab9d213
44240 <44240@PC004145.coll.lr.local>
2014-11-25 13:01:50 +0000

    Updated.

--- Online Bank Registration Functions.py
+++ Online Bank Registration Functions.py
@@ -1,5 +1,6 @@
 #25/11/14
 #Online Bank Registration Program
+#Partner: Hugo
 
 def user_information():
     print("Please enter your details below: ")
@@ -21,7 +22,7 @@ def process_details(first_name, last_name, gender, address, town, postcode):
             title = "Mrs"
             return title
         elif married == "No":
-            title = "Miss"
+            title = "Ms"
             return title
 
 def return_message(title, first_name, last_name):
